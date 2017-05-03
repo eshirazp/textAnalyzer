e971dc3f29f3c9e00e438c4a5ac4e7c5fa5f6382
Elush Shirazpour <eshirazp@gmail.com>
2017-05-02 21:13:47 -0700

    Added comments

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// your code here!
+/* Returns the average word count. Counts the total characters and then
+    divides by the array length
+*/
 function averageCount(textArr) {
   var count = 0;
   for(var i = 0; i < textArr.length; i++) {
@@ -8,6 +10,9 @@ function averageCount(textArr) {
   return (count/textArr.length).toFixed(2);
 }
 
+/* Returns the unique word count. Sorts the array first and then does a 
+    simple compare. Must start count at 1 for this logic
+*/
 function uniqueCount(textArr) {
   var count = 1;
 
@@ -19,14 +24,22 @@ function uniqueCount(textArr) {
   return count;
 }
 
+/* Returns the word count. Just uses the array.length method */
 function wordCount(textArr) {
   return textArr.length;
 }
 
+/* Converts the text area into an array and then returns it. First trims all 
+    leading and trailing whitespace. Then uses RegExp to remove all spaces 
+    together from populating the array 
+*/
 function textToArr(textArea) {
   return textArea.trim().split(/\s+/g);
 }
 
+/* Gets the value within the text form. Calls all functions above and displays
+    that information into the DOM
+*/
 function textAnalyze() {
   $('button').click(function(event) {
     event.preventDefault();
